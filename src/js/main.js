34adec6a9ea2ec8c45b4ec33382e06d22eb22b84
Alexei Mozyrskiy <mozyrski@gmail.com>
2020-10-25 10:54:28 +0300

    implemented a script for tabs into which classes of buttons and tabs are passed

---
+++ src/js/main.js
@@ -0,0 +1,49 @@
+function lpsTabs(arr)
+{
+    // lpsTabs({
+    //     buttonShowAllTabs: false,                                // есть ли кнопка при нажатии на которую будут показаны все раздела табов, то есть items табов, по умолчнию false
+    //     tabsButtonsParentElementClass: 'tab__titles',            // класс-обертка для кнопок табов, на которые будем нажимать
+    //     tabsItemsClass: 'tab__items',                            // класс для каждого раздела табов, группа елементов обединенных для показа одновременно при нажатии кнопки табов
+    //     tabsButtonActiveClass: 'tab__title_active',              // класс активности для кнопки табов
+    //     tabsItemActiveClass: 'tab__items_active',                // класс активности для раздела табов (группы item`ов)
+    // });
+
+    if(typeof arr.buttonShowAllTabs == 'undefined') {               // если разработчик не указал показывать кнопку для показа всех табов примем, что ее показывать не надо
+        arr.buttonShowAllTabs = false;
+    }
+
+    const tabsButtonsParentElement = document.querySelector('.' + arr.tabsButtonsParentElementClass),
+            tabsButtons = tabsButtonsParentElement.children;
+            tabsItems = document.getElementsByClassName(arr.tabsItemsClass);
+    let currentButtonIndex;          // тут будет хранится индекс кликнутого элемента, чтобы понять какому элементу добавлять класс активности. Индекс будет вычисляться в зависимости от того пожелал ли разработчик видеть кнопку для всех items табов
+
+    for(let i = 0; i < tabsButtons.length; i++) {
+        tabsButtons[i].addEventListener('click', function() {  // при клике на любую кнопку
+            for(let tabsButton of tabsButtons) {                // уберем у всех кнопок класс активности
+                tabsButton.classList.remove(arr.tabsButtonActiveClass);
+            }
+
+            for(let tabsItem of tabsItems) {                // уберем у всех разделов табов класс активности
+                tabsItem.classList.remove(arr.tabsItemActiveClass);
+            }
+
+            if(arr.buttonShowAllTabs) {
+                currentButtonIndex = (i - 1);           // если показываем кнопку при клике на которую будут показываться все items табов то индекс item`а табов будет на один меньше чем индекс текущей кнопки
+            } else {
+                currentButtonIndex = i;
+            }
+
+            if(currentButtonIndex < 0) {
+                tabsButtons[0].classList.add(arr.tabsButtonActiveClass); // если нажали на кнопку которыя покажет все разделы табов
+
+                for(let tabsItem of tabsItems) {                // вставим всем разделам табов класс активности
+                    tabsItem.classList.add(arr.tabsItemActiveClass);
+                }
+            } else {
+                tabsButtons[i].classList.add(arr.tabsButtonActiveClass);
+
+                tabsItems[currentButtonIndex].classList.add(arr.tabsItemActiveClass);
+            }
+        });
+    }
+}
